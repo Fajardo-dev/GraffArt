c625e4e89b89671cf11d67b3c8f238451c19b1dd
Fajardo-dev <johnfajardo177@gmail.com>
2024-02-11 17:38:48 -0500

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <h1>Urban</h1>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default Home;
